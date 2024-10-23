69a9e4f42c47fb73d03d1f7df2f5ac24e2d1dafe
Sunarto Utina <nartosunarto73126@gmail.com>
2024-10-23 18:47:45 +0700

    Menampilkan ikon cafe dan menambahkan menu produk

--- laravel-backend-kedai/resources/views/components/sidebar.blade.php
+++ laravel-backend-kedai/resources/views/components/sidebar.blade.php
@@ -3,7 +3,11 @@
         <aside id="sidebar-wrapper">
             <div class="sidebar-brand">
                 <a href="index.html">my-KEDAI</a>
-                {{-- <a href="index.html"><img src="{{ asset('img/resto.png') }}" alt="logo" width="80"></a> --}}
+                <div class="login-brand">
+                    {{-- <img src="{{ asset('img/kedai.png') }}" alt="logo" width="80" class=""> --}}
+
+
+                </div>
             </div>
             <div class="sidebar-brand sidebar-brand-sm">
                 <a href="index.html">MK</a>
@@ -11,16 +15,19 @@
             <ul class="sidebar-menu">
                 <li class="menu-header">Dashboard</li>
                 <li class="{{ Request::is('home*') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ url('home') }}"><i class="fas fa-chart-line"></i><span>Dashboard</span></a>
+                    <a class="nav-link" href="{{ url('home') }}"><i
+                            class="fas fa-chart-line"></i><span>Dashboard</span></a>
                 </li>
                 <li class="menu-header">Users</li>
                 <li class="{{ Request::is('user*') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{route('user.index')}}"><i class="fas fa-user"></i><span>All Users</span></a>
+                    <a class="nav-link" href="{{ route('user.index') }}"><i class="fas fa-user"></i><span>All
+                            Users</span></a>
                 </li>
 
                 <li class="menu-header">Product</li>
                 <li class="{{ Request::is('product*') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{route('user.index')}}"><i class="fas fa-box"></i><span>All Products</span></a>
+                    <a class="nav-link" href="{{ route('user.index') }}"><i class="fas fa-box"></i><span>All
+                            Products</span></a>
 
                 </li>
 
